5100d524bc92103eac0c2c5184f3018afde49b4b
9iFx <41243509+9iFx@users.noreply.github.com>
2018-07-15 08:08:41 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,48 +2,29 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const prefix = '$'
 
-client.on('ready', () => {
+client.on('تم تشغيل', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`Regen,`,"http://twitch.tv/S-F")
-  console.log('')
-  console.log('')
-  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
-  console.log(`[Start] ${new Date()}`);
-  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════════════════════════════]╗');
-  console.log(`Logged in as * [ " ${client.user.username} " ]`);
-  console.log('')
-  console.log('Informations :')
-  console.log('')
-  console.log(`servers! [ " ${client.guilds.size} " ]`);
-  console.log(`Users! [ " ${client.users.size} " ]`);
-  console.log(`channels! [ " ${client.channels.size} " ]`);
-  console.log('╚[════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════]╗')
-  console.log(' Bot Is Online')
-  console.log('╚[════════════]╝')
-  console.log('')
-  console.log('')
 });
 
-client.on('message', message => {
-    if(message.content.startsWith(prefix + 'move all')) {
-     if (!message.member.hasPermission("MOVE_MEMBERS")) return message.channel.send('**لايوجد لديك صلاحية سحب الأعضاء**');
-       if(!message.guild.member(client.user).hasPermission("MOVE_MEMBERS")) return message.reply("**لايوجد لدي صلاحية السحب**");
-    if (message.member.voiceChannel == null) return message.channel.send(`**الرجاء الدخول لروم صوتي**`)
-     var author = message.member.voiceChannelID;
-     var m = message.guild.members.filter(m=>m.voiceChannel)
-     message.guild.members.filter(m=>m.voiceChannel).forEach(m => {
-     m.setVoiceChannel(author)
-     })
-     message.channel.send(`**تم سحب جميع الأعضاء الي الروم الصوتي حقك.**`)
+client.on('voiceStateUpdate', (old, now) => {
+  const channel = client.channels.get('466481150312054785');
+  const currentSize = channel.guild.members.filter(m => m.voiceChannel).size;
+  const size = channel.name.match(/\[\s(\d+)\s\]/);
+  if (!size) return channel.setName(`Voice ⇏ 「 ${currentSize} 」`);
+  if (currentSize !== size) channel.setName(`Voice ⇏ 「 ${currentSize} 」`);
+});
 
-
-     }
-       });
-  
-
+client.on('message', message =>{
+    if (message.author.bot) return;
+    if(message.content == "r!type"){
+message.channel.startTyping();
+}
+});
+client.on('message', message =>{
+    if (message.author.bot) return;
+    if(message.content == "r!type"){
+message.channel.stopTyping();
+}
+});
 
 client.login(process.env.BOT_TOKEN);
